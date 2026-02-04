cf120f10927dfa3a3e27a1f7304e4567caea7131
montasirabhi <montasirrahmanabhi@gmail.com>
2026-02-04 20:23:38 +0600

    Keywords

--- fristprog.py
+++ fristprog.py
@@ -57,3 +57,16 @@ a = None
 print(type(Boll))
 print(type(old))
 print(type(a))
+
+
+# ==================================================
+
+# Keywords : ( Reserved Words In Python)
+# and ,as, assert , break , class , continue , def , del , elif 
+# else , except , finally , False , for , from , global , if , import
+# in , is, lambda, nonlocal , None , not , or , pass , raise
+# return , True , try , with , while , yield
+
+
+# Keywords Can not be used for variables 
+
