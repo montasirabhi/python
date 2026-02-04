8513a30e50c42c4179a9cba1af350394b88a3311
montasirabhi <montasirrahmanabhi@gmail.com>
2026-02-04 19:37:24 +0600

    Sum And Multiple Sentence

--- fristprog.py
+++ fristprog.py
@@ -1,4 +1,8 @@
 print("Hello World")
-
+print("I Am MD MONTASIR RAHMAN ABHI")
+print("My Age Is 22")
+print("Frist Sentence .","Secound Sentence")
+print(23)
+print(23+32)
 # Print Is A Function , it's use for printing Something On The Screen
 #Hello World is an text what we wanted to Show on hour Screen . And Cotations Are Use For Print String 
