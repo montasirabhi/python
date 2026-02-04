45ca84bc889134ffbd503de71b7e9189f9b21bac
montasirabhi <montasirrahmanabhi@gmail.com>
2026-02-04 19:30:15 +0600

    Comment Updated Line 4

--- fristprog.py
+++ fristprog.py
@@ -1,3 +1,4 @@
 print("Hello World")
 
 # Print Is A Function , it's use for printing Something On The Screen
+#Hello World is an text what we wanted to Show on hour Screen . And Cotations Are Use For Print String 
