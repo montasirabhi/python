077dac95064034275e9082998af9676d84bd15b2
montasirabhi <montasirrahmanabhi@gmail.com>
2026-02-04 19:56:10 +0600

    Variable Primary Added

--- fristprog.py
+++ fristprog.py
@@ -1,8 +1,25 @@
+# Print Is A Function , it's use for printing Something On The Screen
+#Hello World is an text what we wanted to Show on hour Screen . And Cotations Are Use For Print String 
+
 print("Hello World")
 print("I Am MD MONTASIR RAHMAN ABHI")
 print("My Age Is 22")
 print("Frist Sentence .","Secound Sentence")
 print(23)
 print(23+32)
-# Print Is A Function , it's use for printing Something On The Screen
-#Hello World is an text what we wanted to Show on hour Screen . And Cotations Are Use For Print String 
+
+#Variables
+# A Variable is a name given to a memory location in a program.
+
+name = "Abhi" #string
+age = 22 # Intiger
+price = 25.50 # Floating
+
+# The name , age and price is the variables and Abhi , 23 , 25.50 are values
+
+print(name)
+print(age)
+print(price)
+
+print ("Hello World ! My Name is ", name ,"And My Age is", age , "Todays Patato Rate is", price)
+# The Comma Is Very Important to add tow thing in one line.
