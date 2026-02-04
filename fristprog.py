6b92b4ed18006cc05b6d7340ac51332de624eadb
montasirabhi <montasirrahmanabhi@gmail.com>
2026-02-04 20:05:41 +0600

    Identifier Intro

--- fristprog.py
+++ fristprog.py
@@ -14,12 +14,25 @@ print(23+32)
 name = "Abhi" #string
 age = 22 # Intiger
 price = 25.50 # Floating
+age2 = age
 
 # The name , age and price is the variables and Abhi , 23 , 25.50 are values
+# A variable can store another variable what have a value
 
 print(name)
 print(age)
 print(price)
 
-print ("Hello World ! My Name is ", name ,"And My Age is", age , "Todays Patato Rate is", price)
+print ("Hello World ! My Name is ", name ,". And My Age is", age , ". Todays Patato Rate is", price)
+print(age2)
 # The Comma Is Very Important to add tow thing in one line.
+
+
+#Rules Of Identifiers
+# 1) Identifires can be combination of uppercase and lowercase latters , digit or underscore(_)
+# So my_Variable , variable_1, variable_for_print all are valid python identifires.
+# An Identifires can not start with digit . So while variable1 is valid and 1variable is invalid.
+# We can't use special symbols like !,#,@,%,$ etc in our identifier.
+# Identifier can ve of any length.
+
+ 
